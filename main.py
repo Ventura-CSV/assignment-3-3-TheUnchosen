c6488d675687cc994110d1db8de85e73cd020047
TheUnchosen <longbui369@gmail.com>
2024-06-28 18:43:04 +0000

    1212212

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ def main():
     result = True
     if email[0].isalpha:
         result = False
-    if email.len < 5 or email.len > 30:
+    if len(email) < 5 or len(email) > 30:
         result = False
     if email.find("@") == -1:
         result = False
     if email.find(".") == -1 or (email.find("@") > email.find(".")):
         result = False
-    
+        
     
     if result == False:
         print("this is for testing, delete before submission")
